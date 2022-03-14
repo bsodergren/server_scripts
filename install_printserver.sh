b7af64e09f66894e4a0897b5d018c6be3cb40247
bjorn sodergren <bjorn.sodergren@gmail.com>
2022-03-14 05:51:59 +0000

    stuff

--- install_printserver.sh
+++ install_printserver.sh
@@ -1,10 +1,9 @@
-#! /bin/sh
+#!/bin/bash
 HOME_DIR="/home/pi"
 SOURCE_DIR="${HOME_DIR}/repo/server_scripts"
 
 function addGit()
 {
-    
 
 cat > "${HOME_DIR}/.git-credentials" <<EOL
 https://bsodergren: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@github.com
@@ -24,35 +23,34 @@ EOL
 
     mkdir "${HOME_DIR}/repo"
     cd "${HOME_DIR}/repo"
-    
+
     git clone https://github.com/bsodergren/server_scripts.git
 }
 
 
 function addPlugins()
 {
-    
     while read -r line
     do
         ${HOME_DIR}/oprint/bin/pip install $line
 
     done < "${SOURCE_DIR}/config/plugins.list"
-    
 }
+
 function addAliases()
 {
-    if [ ! -e "${HOME_DIR}/.bash_aliases" ] then;
+    if [ ! -f "${HOME_DIR}/.bash_aliases" ]; then
         cp "${SOURCE_DIR}/config/aliases.bash" "${HOME_DIR}/.bash_aliases"
-        . ~/.bashrc
+        source ~/.bashrc
     fi
 }
 
 function addSambaConf()
 {
     sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bk
-    sudo cat ${SOURCE_DIR}/config/samba.conf > /etc/samba/smb.conf
+    sudo cp ${SOURCE_DIR}/config/samba.conf > /etc/samba/smb.conf
 
-    if [ -f "${SOURCE_DIR}/install_wsdd.sh" ] then;
+    if [ -f "${SOURCE_DIR}/install_wsdd.sh" ]; then
         . ${SOURCE_DIR}/install_wsdd.sh
     fi
 
@@ -62,25 +60,25 @@ function addSambaConf()
 
 function updateApt()
 {
-    
-    sudo apt-get update
-    sudo apt-get -y upgrade
+
+#    sudo apt-get update
+#    sudo apt-get -y upgrade
     # Install Git:
-    sudo apt-get -y install git exa  samba  samba-common samba-common-bin 
+    sudo apt-get -y install git exa  samba  samba-common samba-common-bin
 
 }
 
 updateApt
 addGit
 
-if [ -d ${SOURCE_DIR} ] then;
+if [ -d "${SOURCE_DIR}" ]; then
     cd "${SOURCE_DIR}"
 
     addSambaConf
     addAliases
     addPlugins
-   
-fi 
+fi
+
 
 
 
