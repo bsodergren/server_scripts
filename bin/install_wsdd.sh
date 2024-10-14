40a3a13e3d07fb174edddf4ec0299388774a9c96
bjorn <bjorn.sodergren@gmail.com>
2024-10-14 21:00:47 +0000

    update

--- bin/install_wsdd.sh
+++ bin/install_wsdd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo cp wsdd /usr/local/bin/wsdd
-sudo cp wsdd.service /etc/systemd/system/
+sudo cp ../src/wsdd /usr/local/bin/wsdd
+sudo cp ../src/wsdd.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable wsdd.service
 sudo systemctl start wsdd.service
